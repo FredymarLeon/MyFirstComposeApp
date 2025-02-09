f8c950ac06d33e7d17c8cb13ed7b5511599f4595
FredymarLeon <fredymar@gmail.com>
2025-02-09 18:01:08 -0300

    Haciendo el primer desafio:
    Cree un nuevo fichero llamado Ejercicio1.kt. En él cree una función composablellamada Ejercicio1, le coloque el modifier: Modifier como parametro y lo llame en la MainActivity.kt dentro de Scaffold.
     Luego comenzé a construir el Layout:
     Utilize una Columna(pasando el modifier que recibi por parametro -> modifier = modifier) y tres Boxcom um peso de 1f y fillMaxSize cada uno.
    
      Al primer Box le coloqué un background y dentro coloqué  un Text centrado (contentAligment = Aligment.Center).
    
      Dentro del segundo Box coloqué una Row y dentro de ella dos Box(después percibí que pude haber sustituido el Box por una Row), seguidamente coloqué dos Box dentro de ella, com peso de 1f cada uno y una altura de 290.dp, asi como um background. En el tercer Box coloqué un texto centralizado.
    
      En el tercer box, hice basicamente lo mismo que en el primer box.

--- app/src/main/java/com/fredymar/myfirstcomposeapp/components/layout/MYComplexLayout.kt
+++ app/src/main/java/com/fredymar/myfirstcomposeapp/components/layout/MYComplexLayout.kt
@@ -7,13 +7,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
-import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
 
 @Composable
